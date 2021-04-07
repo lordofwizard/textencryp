188a180ad117845446e19f287010447945212ee1
lordofwizard <lordofwizard@github.com>
2021-04-07 22:19:10 +0530

    added the python functionality thanks to spyd

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-ascChar = input("Mesasge :  ")
-finalChar = list(ascChar)
-for i in range(0,len(ascChar)):
-    x = ord(finalChar[i])
-    if x > 64 and x < 78:
-        x+=13
-    elif x > 78 and x < 90:
-        x-=13
-    elif x > 96 and x < 110:
-        x+=13
-    elif x > 109 and x < 123:
-        x-=13
-        finalChar[i] = chr(x)
+msg = input("Message:\t")               #input
+counter = 0                             #just for for loop
+msg = list(msg)                         #needs to be converted into an array first -_-
 
-for j in range(0,len(ascChar)):
-    mainresult = ''.join(finalChar[j])
-print(mainresult)
-#you are very dumb if you need explaination of this stuff
+for lascii in msg:
+    lascii = ord(lascii)                #convert into ascii
+    if lascii > 64 and lascii < 78:
+        lascii = lascii + 13
+    elif lascii > 78 and lascii < 90:
+        lascii = lascii - 13
+    elif lascii > 96 and lascii < 110:
+        lascii = lascii + 13
+    elif lascii > 109 and lascii < 123:
+        lascii = lascii - 13
+    msg[counter] = chr(lascii)          #reconvert into string
+    counter = counter + 1               #array thingy
+
+msg = ''.join(msg)                      #conversion into str again
+print("\nEncrypted:\t",msg)
+#code can be even more elegant and simpler but I am in hell so don't have power to learn anything >_<
